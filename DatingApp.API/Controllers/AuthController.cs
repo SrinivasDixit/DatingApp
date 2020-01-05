ece3fa5c7a0f809d6dcb131133df39f5e4862f63
SrinivasDixit <srini.dixit@gmail.com>
2020-01-05 15:57:43 +0530

    End of section3

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _config = config;
+            _repo = repo;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+        {
+            userForRegisterDTO.UserName = userForRegisterDTO.UserName.ToLower();
+            if (await _repo.UserExists(userForRegisterDTO.UserName))
+                return BadRequest("User already exists");
+            var userToCreate = new User()
+            {
+                UserName = userForRegisterDTO.UserName
+            };
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);
+            return StatusCode(201);
+
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            var user = await _repo.Login(userForLoginDTO.UserName.ToLower(), userForLoginDTO.Password);
+            if (user == null)
+                return Unauthorized();
+            var claims = new[]{
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Name,user.UserName)
+            };
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.
+            GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new
+            {
+                Token = tokenHandler.WriteToken(token)
+            });
+        }
+    }
+}
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace DatingApp.API
 {
     public class Startup
@@ -27,9 +31,23 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x=>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.
+                    GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer=false,
+                    ValidateAudience=false
+                };
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +61,8 @@ namespace DatingApp.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseCors(a=>a.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(a => a.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
